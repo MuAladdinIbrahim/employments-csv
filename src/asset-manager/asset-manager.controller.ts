07e039d95b3a269f9a3456799185202c87e6c437
MuAladdinIbrahim <muhammadalaa27@gmail.com>
2023-09-19 22:46:34 +0300

    feat: memoryStorage instead

--- src/asset-manager/asset-manager.controller.ts
+++ src/asset-manager/asset-manager.controller.ts
@@ -9,7 +9,8 @@ import {
   MaxFileSizeValidator,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
+import * as fs from 'fs';
+import { diskStorage, memoryStorage } from 'multer';
 
 @Controller('asset')
 export class AssetManagerController {
@@ -17,14 +18,15 @@ export class AssetManagerController {
   @Post('upload')
   @UseInterceptors(
     FileInterceptor('file', {
-      storage: diskStorage({
-        destination: './uploads/files',
-        filename: (req, file, cb) => {
-          const fileName = file.originalname.split('.')[0];
-          const fileExtName = file.originalname.split('.')[1];
-          cb(null, `${fileName}-${new Date().getTime()}.${fileExtName}`);
-        },
-      }),
+      storage: memoryStorage(),
+      // storage: diskStorage({
+      //   destination: './uploads/files',
+      //   filename: (req, file, cb) => {
+      //     const fileName = file.originalname.split('.')[0];
+      //     const fileExtName = file.originalname.split('.')[1];
+      //     cb(null, `${fileName}-${new Date().getTime()}.${fileExtName}`);
+      //   },
+      // }),
     }),
   )
   async uploadFile(
@@ -42,6 +44,23 @@ export class AssetManagerController {
       this.logger.debug(
         `Uploading file..., ${file.originalname} as ${file.filename}`,
       );
+      this.logger.debug(`File size: ${file.size}}`);
+      fs.mkdirSync('./uploads/files', { recursive: true });
+      fs.appendFile(
+        `./uploads/files/${new Date().getTime()}-${file.originalname.replace(
+          /\ /g,
+          '-',
+        )}`,
+        file.buffer,
+        { encoding: 'utf-8' },
+        (err) => {
+          if (err) {
+            this.logger.error(err);
+            throw err;
+          }
+          this.logger.debug(`File ${file.filename} saved successfully`);
+        },
+      );
       return {
         message: 'File uploaded successfully 🎉',
       };
@@ -51,3 +70,13 @@ export class AssetManagerController {
     }
   }
 }
+
+/* TODO tradeoffs of using memory storage vs disk storage
+  when using disk storage in inceptor, incetor will save the file to disk before checking the validators as
+  inceptor is executed before the pipe
+  when using memory storage, inceptor will save the file to memory and we can use the buffer later and fs
+  to write the file to disk
+
+  you can use disk memory and in catch clause of the controller, you can delete the file from disk, but
+  you risk of using fs in catch clause!
+*/
